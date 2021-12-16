084fb2df1abe19417e5440622aae3d1cdb6f50b5
MHarisMunir <harismunir030@gmail.com>
2021-12-16 23:59:07 +0500

    composer auto-loader working

--- public/index.php
+++ public/index.php
@@ -7,21 +7,26 @@
 // Require the controller class
 //require '../App/Controllers/Posts.php';
 
+//Composer
+
+require '../vendor/autoload.php';
+
 /**
  * Twig
  */
-require_once dirname(__DIR__) . '/vendor/Twig/lib/Twig/Autoloader.php';
+//require_once dirname(__DIR__) . '/vendor/Twig/lib/Twig/Autoloader.php';
+
 Twig_Autoloader::register();
 
  // Autoloader
- 
+ /*
 spl_autoload_register(function ($class) {
     $root = dirname(__DIR__);   // get the parent directory
     $file = $root . '/' . str_replace('\\', '/', $class) . '.php';
     if (is_readable($file)) {
         require $root . '/' . str_replace('\\', '/', $class) . '.php';
     }
-});
+});*/
 
 //Routing
 //
